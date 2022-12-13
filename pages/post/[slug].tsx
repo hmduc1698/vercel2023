4acbae08df4552cf6b07627ea5eb1413d00b7ca5
jacktran46 <63067327+jacktran46@users.noreply.github.com>
2022-12-13 11:23:32 +0700

    Update [slug].tsx

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,33 +1,40 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Head from 'next/head'
-import { GetStaticPaths, GetStaticProps } from 'next'
-import Container from '../../components/container'
-import PostBody from '../../components/post-body'
-import MoreStories from '../../components/more-stories'
-import Header from '../../components/header'
-import PostHeader from '../../components/post-header'
-import SectionSeparator from '../../components/section-separator'
-import Layout from '../../components/layout'
-import PostTitle from '../../components/post-title'
-import Tags from '../../components/tags'
-import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
-import { CMS_NAME } from '../../lib/constants'
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Head from "next/head";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Container from "../../components/container";
+import PostBody from "../../components/post-body";
+import MoreStories from "../../components/more-stories";
+import Header from "../../components/header";
+import PostHeader from "../../components/post-header";
+import SectionSeparator from "../../components/section-separator";
+import Layout from "../../components/layout";
+import PostTitle from "../../components/post-title";
+import Tags from "../../components/tags";
+import { getAllPostsWithSlug, getPostAndMorePosts } from "../../lib/api";
+import { CMS_NAME } from "../../lib/constants";
+import { collapseTextChangeRangesAcrossMultipleVersions } from "typescript";
 
+const domain = process.env.NEXT_PUBLIC_WORDPRESS_API_URL.replace("graphql", "");
 
-const domain = process.env.NEXT_PUBLIC_WORDPRESS_API_URL.replace('graphql', '');
-
-export default function Post({ post, preview }) {
+export default function Post({ post, preview, host }) {
   const router = useRouter();
 
-  const isRedirect = (typeof window !== "undefined" && (window.location.search || (typeof document !== "undefined" && document.referrer.indexOf("facebook.com") !== -1)) && post.slug) ? true : false;
+  const isRedirect =
+    typeof window !== "undefined" &&
+    (window.location.search ||
+      (typeof document !== "undefined" &&
+        document.referrer.indexOf("facebook.com") !== -1)) &&
+    post.slug
+      ? true
+      : false;
 
   if (isRedirect) {
     window.location.href = `${domain}${post.slug}`;
   }
 
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
 
   return (
@@ -40,43 +47,56 @@ export default function Post({ post, preview }) {
           <>
             <article>
               <Head>
-                <title>
-                  {post.title}
-                </title>
                 <meta
-                  property="og:title"
-                  content={post.title}
-                />
-                <meta
-                  property="og:image"
-                  content={post.featuredImage?.node.sourceUrl}
+                  property="og:url"
+                  content={`${host}/_next/image?url=${encodeURIComponent(
+                    post.featuredImage?.node.sourceUrl
+                  )}&w=3840&q=100`}
                 />
               </Head>
-              <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">{post.title}</h1>
-              <p>You are being redirected to the post, please wait 1-2 seconds...</p>
+              <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+                {post.title}
+              </h1>
+              <p>
+                You are being redirected to the post, please wait 1-2 seconds...
+              </p>
             </article>
           </>
         )}
       </Container>
     </Layout>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
   const { slug } = context.params;
 
-  if ((context?.req?.headers?.referer || "").indexOf("facebook.com") !== -1) {
-    context.res.setHeader("location", `${domain}${slug}`);
-    context.res.statusCode = 301;
-    context.res.end();
-    return { props: { data: {} } };
+  // If slug is id
+  if (context.req.headers.referer && Number.isInteger(Number(slug))) {
+    if (context.req.headers.referer.indexOf("facebook.com") !== -1) {
+      context.res.setHeader("location", `${domain}?p=${slug}`);
+      context.res.statusCode = 301;
+      context.res.end();
+      return {
+        props: {
+          preview: false,
+          post: {},
+        },
+      };
+    }
   }
-  const data = await getPostAndMorePosts(slug, false, {})
+  
+  // console.log("context.req.headers.host---", context.req.headers.host);
+  const data = await getPostAndMorePosts(slug, false, {});
 
   return {
     props: {
       preview: false,
       post: data.post,
-    }
-  }
+      host:
+        context.req.headers.host.indexOf("localhost") !== -1
+          ? `http://${context.req.headers.host}`
+          : `https://${context.req.headers.host}`,
+    },
+  };
 }
